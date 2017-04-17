7af27444f2b0e15c482686de30c531154cfd7a35
Victor <viktorgonzalezv123@gmail.com>
2017-04-17 23:43:08 +0100

    Requesting user permissions

--- app/src/main/java/com/example/vctor/findme/MainActivity.java
+++ app/src/main/java/com/example/vctor/findme/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.vctor.findme;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+    private BluetoothManager btManager;
+    private BluetoothAdapter btAdapter;
+    private static final int REQUEST_ENABLE_BT = 735;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // init BLE
+        btManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        btAdapter = btManager.getAdapter();
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        if (btAdapter == null || !btAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
     }
     //Code goes here...
 }
