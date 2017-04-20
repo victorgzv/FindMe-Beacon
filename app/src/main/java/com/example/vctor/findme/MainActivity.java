944fe0b0f308513c441f2228783dc0b6ac444c93
Victor <viktorgonzalezv123@gmail.com>
2017-04-21 00:22:18 +0100

    Update toolbar

--- app/src/main/java/com/example/vctor/findme/MainActivity.java
+++ app/src/main/java/com/example/vctor/findme/MainActivity.java
@@ -1,58 +1,68 @@
 package com.example.vctor.findme;
 
-import android.app.Activity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.RemoteException;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-import org.altbeacon.beacon.BeaconConsumer;
-import org.altbeacon.beacon.BeaconManager;
-import org.altbeacon.beacon.MonitorNotifier;
-import org.altbeacon.beacon.Region;
 
-public class MainActivity extends Activity implements BeaconConsumer {
-    protected static final String TAG = "MainActivity";
-    private BeaconManager beaconManager;
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mDeviceListRecyclerView;
+    private static final long SCAN_PERIOD = 10000;
+    private boolean mScanning;
+    private Handler mHandler = new Handler();
+    private BluetoothAdapter mBtAdapter;
+    private BluetoothLeScanner mBtScanner;
+    private static final int REQUEST_ENABLE_BT = 3;
+    private static final int REQUEST_PERMISSION_FINE = 163;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(TAG, "App started up");
-        beaconManager = BeaconManager.getInstanceForApplication(this);
-        // To detect proprietary beacons, you must add a line like below corresponding to your beacon
-        // type.  Do a web search for "setBeaconLayout" to get the proper expression.
-        // beaconManager.getBeaconParsers().add(new BeaconParser().
-        //        setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
-        beaconManager.bind(this);
+
+         Toolbar mToolbar= (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
     }
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        beaconManager.unbind(this);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu, menu);
+        return true;
     }
+
     @Override
-    public void onBeaconServiceConnect() {
-        beaconManager.addMonitorNotifier(new MonitorNotifier() {
-            @Override
-            public void didEnterRegion(Region region) {
-                Log.i(TAG, "I just saw an beacon for the first time!");
-            }
-
-            @Override
-            public void didExitRegion(Region region) {
-                Log.i(TAG, "I no longer see an beacon");
-            }
-
-            @Override
-            public void didDetermineStateForRegion(int state, Region region) {
-                Log.i(TAG, "I have just switched from seeing/not seeing beacons: "+state);
-            }
-        });
-
-        try {
-            beaconManager.startMonitoringBeaconsInRegion(new Region("myMonitoringUniqueId", null, null, null));
-        } catch (RemoteException e) {    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.action_bluetooth){
+            initBluetooth();
+        }
+        return super.onOptionsItemSelected(item);
     }
+    void initBluetooth() {
+        //        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+        //the following method is recommended to use for getting BluetoothAdapter
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBtAdapter = bluetoothManager.getAdapter();
 
+        if (mBtAdapter == null || !mBtAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+        }
+        mBtScanner = mBtAdapter.getBluetoothLeScanner();
+
+
+    }
 }
