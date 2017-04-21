bdb2cff5b724e8c2ef24154a265938ac29231fb7
Victor <viktorgonzalezv123@gmail.com>
2017-04-21 14:55:56 +0100

    Added link to Register Activity

--- app/src/main/java/com/example/vctor/findme/RegisterActivity.java
+++ app/src/main/java/com/example/vctor/findme/RegisterActivity.java
@@ -2,6 +2,8 @@ package com.example.vctor.findme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText etUserName= (EditText) findViewById(R.id.etLoginUser);
+        final EditText etName= (EditText) findViewById(R.id.etName);
+        final EditText etEmail= (EditText) findViewById(R.id.etEmail);
+        final EditText etPwd= (EditText) findViewById(R.id.etPassword);
+        final Button btnRegister= (Button) findViewById(R.id.btnRegister);
     }
 }
